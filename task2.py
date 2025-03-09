d591d5abc5aacf5b5a2fee4bd66b4bd6e2e2a517
sharif-08 <otwist731@gmail.com>
2025-03-09 12:57:30 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,8 @@
+import math
+user_input=int(input("enter a number: "))
+square=math.sqrt(user_input)
+print("square root: ",square)
+log=math.log(user_input)
+print("logrithum: ",log)
+sin=math.sin(user_input)
+print("sine: ",sin)
